f762e55a06e96a33513c74526670717ea522f099
Ca_String <considerate76@gmail.com>
2025-12-30 22:01:39 +0900

    Inital Project Template

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+	globalIgnores(['dist', 'node_modules']),
+
+	{
+		files: ['**/*.{ts,tsx,js,jsx}'],
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: globals.browser,
+		},
+		plugins: {
+			import: importPlugin,
+		},
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ varsIgnorePattern: '^[A-Z_]' }
+			],
+		},
+		settings: {
+			'import/resolver': {
+				typescript: {
+					project: './tsconfig.json'
+				}
+			}
+		}
+	},
+])
